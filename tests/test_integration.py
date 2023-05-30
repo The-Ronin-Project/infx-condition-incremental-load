88951269fcbc6df96004940b5259ea6bbd490ba3
Rey Johnson <rey@projectronin.com>
2023-05-30 13:53:44 -0600

    Outline for integration test

---
+++ tests/test_integration.py
@@ -0,0 +1,20 @@
+
+def generate_sample_error():
+    """
+    Each time the test is run, sample data needs to be created.
+    We will use 'Test Concept [timestamp]' as the display, so it's unique every time.
+    :return:
+    """
+    pass
+
+def test_incremental_load_integration():
+    # Set up the mock error response
+
+    # Run the process
+
+    # Verify new code is in code system
+
+    # Verify new code is in value set
+
+    # Verify new code is in concept map
+    pass
